19b3ced96254c87b1901a7992fb6781d5bfa976b
deven <devendraa769@gmail.com>
2023-12-24 14:24:17 +0545

    list implementation

---
+++ list.py
@@ -0,0 +1,29 @@
+
+
+fruits = ['apple', 'banana']
+another_fruit = ['litchi','guva']
+
+print(fruits.extend(another_fruit))
+print(fruits)
+print(combined_list)
+fruits.append('mango')
+print(fruits)
+print(help(fruits))
+for index, value in enumerate(fruits):
+	print(index, value)
+
+print(fruits[0:4:3])
+print(fruits[0:2])
+
+for banana in fruits:
+
+print(fruits[-2:-1])
+
+
+
+# print(help(fruits))
+# print(type(fruits))
+
+
+# carList = ["Volvo", "Toyata"]
+# carList[1] = 'Farari'
